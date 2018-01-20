08857be21621d1d2b34b533e8b552274ef3df5d2
XAS-712 <xas712@126.com>
2018-01-20 08:23:36 +0800

    commit

--- markdown.py
+++ markdown.py
@@ -3,5 +3,30 @@
 def header(text,level):
     level=max(1,level)
     level=min(6,level)
-    result='#'*level + ' ' + text
+    result='#'*level+' '+text
     return result;
+
+def bold(text):
+    result='**'+text+'**'
+    return result
+
+def italic(text):
+    result='*'+text+'*'
+    return result
+
+def delline(text):
+    result='~~'+text+'~~'
+    return result
+
+def b(text):
+    return bold(text)
+
+def i(text):
+    return italic(text)
+
+def d(text):
+    return delline(text)
+
+def quote(text):
+    result='> '+text
+    return result
